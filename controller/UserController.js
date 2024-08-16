98e240fc7b1d1f2f6be206bfb45e3e6dace37b32
Erin-Caitlin <erinwilliamscat@gmail.com>
2024-08-16 11:39:54 +0200

    Updates

--- controller/UserController.js
+++ controller/UserController.js
@@ -14,10 +14,10 @@ userRouter.get('/:id', (req, res) => {
 userRouter.post('/register', (req, res) => {
     users.registerUser(req, res)
 })
-userRouter.patch('/user/:id', (req, res) => {
+userRouter.patch('/:id', (req, res) => {
     users.updateUser(req, res)
 })
-userRouter.delete('/user/:id', (req, res) => {
+userRouter.delete('/:id', (req, res) => {
     users.deleteUser(req, res)
 })
 userRouter.post('/login', (req, res) => {
